6a7832d6ff93a8050105f43d7c2d0e3c3e43dc27
Baran Büyük <baranbuyuk@gmail.com>
2020-11-28 09:53:19 +0300

    Changed code.

--- Week-4/src/org/kodluyoruz/examples/Main.java
+++ Week-4/src/org/kodluyoruz/examples/Main.java
@@ -15,7 +15,7 @@ public class Main {
      *              HashSet
      *              LinkedHashSet
      *              SortedSet
-     *              TreeSet
+     *                  TreeSet
      *          Queue
      *
      **/
