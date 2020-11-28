cf6e75677135976095d449fc663d01030741c426
Baran Büyük <baranbuyuk@gmail.com>
2020-11-28 08:18:39 +0300

    Added Week-4

---
+++ Week-4/src/org/kodluyoruz/examples/Main.java
@@ -0,0 +1,34 @@
+package org.kodluyoruz.examples;
+
+
+public class Main {
+
+    /**
+     * Iterable --
+     *      Collection
+     *          List
+     *              ArrayList
+     *              LinkedList
+     *              Vector
+     *              Stack
+     *          Set
+     *              HashSet
+     *              LinkedHashSet
+     *              SortedSet
+     *              TreeSet
+     *          Queue
+     *
+     **/
+
+    /**
+     * Map
+     * HashMap
+     * LinkedHashMap
+     * SortedMap
+     * TreeMap
+     */
+    public static void main(String[] args) {
+
+    }
+}
+
